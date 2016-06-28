3a5b321d134ef0b6669184132e9ce36b30bcd95b
AnthonyMa0101 <[anthony.ma111@gmail.com]>
2016-06-28 16:59:05 -0400

    Created Project

---
+++ Contact List/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Contact List
+//
+//  Created by Anthony Ma on 2016-06-28.
+//  Copyright © 2016 Anthony Ma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
